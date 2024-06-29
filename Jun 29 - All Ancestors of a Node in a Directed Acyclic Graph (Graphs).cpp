3d4ee4c07d1d6bb7df5abe01ca502b6f29e79c02
Naga <nptbilla@gmail.com>
2024-06-29 10:00:00 +0530

    Jun 29

--- Jun 29 - All Ancestors of a Node in a Directed Acyclic Graph (Graphs).cpp
+++ Jun 29 - All Ancestors of a Node in a Directed Acyclic Graph (Graphs).cpp
@@ -1,15 +1,36 @@
-long long maximumImportance(int n, vector<vector<int>> &roads) {
-    vector<int> v1(n, 0);
-    for (auto road : roads)
+vector<vector<int>> getAncestors(int n, vector<vector<int>> &edges) {
+    vector<vector<int>> adj(n);
+    vector<set<int>> ans(n);
+    vector<int> indegree(n, 0);
+    for (auto edge : edges)
     {
-        v1[road[0]]++;
-        v1[road[1]]++;
+        adj[edge[0]].push_back(edge[1]);
+        indegree[edge[1]]++;
     }
-    sort(v1.rbegin(), v1.rend());
-    long long ans = 0;
+    queue<int> q1;
     for (int i = 0; i < n; i++)
     {
-        ans += (long long)(n - i) * v1[i];
+        if (indegree[i] == 0)
+            q1.push(i);
     }
-    return ans;
+    while (!q1.empty())
+    {
+        int node = q1.front();
+        for (auto adj_node : adj[node])
+        {
+            indegree[adj_node]--;
+            ans[adj_node].insert(ans[node].begin(), ans[node].end());
+            ans[adj_node].insert(node);
+            if (indegree[adj_node] == 0)
+                q1.push(adj_node);
+        }
+        q1.pop();
+    }
+    vector<vector<int>> v1;
+    for (auto s1 : ans)
+    {
+        vector<int> temp(s1.begin(), s1.end());
+        v1.push_back(temp);
+    }
+    return v1;
 }
